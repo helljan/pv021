b1e13fd090479a5b2667d14ad00f4e2e1464a16c
Jan Hellar <janhellar@gmail.com>
2015-10-07 16:16:46 +0200

    init

---
+++ src/main/java/com/mycompany/neuron/main.java
@@ -0,0 +1,12 @@
+package com.mycompany.neuron;
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
